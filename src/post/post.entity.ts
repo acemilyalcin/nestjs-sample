94565ad51669bab880caa2e0d782f894c63ad308
Cemil YALCIN <acemilyalcin@gmail.com>
2021-07-01 23:00:55 +0300

    Post repository creation

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class PostEntity {
+export class Post {
   @PrimaryGeneratedColumn('rowid')
   id: string;
 
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post.controller';
+import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostRepository])],
   controllers: [PostController],
   providers: [PostService],
 })
